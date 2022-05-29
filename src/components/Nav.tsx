d3a5f23f11a2484f66ac3ffd42beecbb02a3fbed
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-05-29 17:26:37 +0200

    fix z-index bug

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "./ui/NavLink";
 const links = ["Work", "Links", "Resume"] as const;
 export const Nav: Component = () => {
   return (
-    <nav class="w-full sticky top-0 bg-gray-50 pt-4 pb-2 -mt-4 border-b h-12">
+    <nav class="w-full sticky top-0 bg-gray-50 pt-4 pb-2 -mt-4 border-b h-12 z-10">
       <ul class="flex justify-between w-full font-bold font-['Big_Caslon'] tracking-widest">
         <For each={links}>{(link) => <NavLink>{link}</NavLink>}</For>
       </ul>
