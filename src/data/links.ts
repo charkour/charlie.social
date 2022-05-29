6ee41c34125d9058d05cff955202e5b25dc9e7ef
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-05-29 15:54:13 +0200

    add project and link data

---
+++ src/data/links.ts
@@ -0,0 +1,28 @@
+export interface SocialLink {
+  href: string;
+  title: string;
+  color: string;
+}
+
+export const links = [
+  {
+    href: "https://www.github.com/charkour",
+    title: "GitHub",
+    color: "Black",
+  },
+  {
+    href: "",
+    title: "LinkedIn",
+    color: "Teal",
+  },
+  {
+    href: "",
+    title: "Twitter",
+    color: "lightblue",
+  },
+  {
+    href: "",
+    title: "Stack Overflow",
+    color: "orange",
+  },
+];
---
+++ src/data/projects.ts
@@ -0,0 +1,56 @@
+import { Project } from "../components/Project";
+
+export const projects: Project[] = [
+  {
+    title: "Tekton.com",
+    description: "ecommerce doing MM in monthly D2C revenue",
+    tech: ["Next.js", "React.js"],
+  },
+  {
+    title: "Course Schedulizer",
+    description:
+      "Project used internally at Calvin University by department chairs",
+    tech: ["React.js", "Sass"],
+  },
+  {
+    title: "Zundo",
+    description: "middleware for zustand. add time travel to your apps",
+    tech: ["TypeScript", "React.js"],
+  },
+  {
+    title: "React Reactions",
+    description:
+      "Fork of an old npm package updated to work with modern React.js and Next.js",
+    tech: ["React.js"],
+  },
+  {
+    title: "Benchmark",
+    description: "Internal app used at CQL",
+    tech: [".NET Core", "C#", "React.js", "SQL Server"],
+  },
+  {
+    title: "Turtle Tracker",
+    description: "Mobile Application",
+    tech: ["React Native", "Expo", "Firebase", "PostgreSQL", "Ionic"],
+  },
+  {
+    title: "IT Inventory Tracker",
+    description: "Internal app used at CQL",
+    tech: [".NET Core", "C#", "React.js"],
+  },
+  {
+    title: "Vacation Tracker",
+    description: "Restyled internal CQL app",
+    tech: ["Bulma", "Gulp"],
+  },
+  {
+    title: "Grit Leadership",
+    description: "Coaching Evaluation",
+    tech: ["TypeScript", "React.js", "MUI"],
+  },
+  {
+    title: "Calvin Orientation",
+    description: "Mobile app",
+    tech: ["TypeScript", "React Native", "Expo"],
+  },
+];
