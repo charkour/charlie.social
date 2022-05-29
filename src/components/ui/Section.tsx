eac8c685aee5ccd6dd2b14562685979c5acad887
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-05-29 14:54:33 +0200

    add Section component

---
+++ src/components/ui/Section.tsx
@@ -0,0 +1,13 @@
+import { type Component, type JSX } from "solid-js";
+import { Text } from './Text';
+
+export const Section: Component<{ title: string; children: JSX.Element }> = (
+  props
+) => {
+  return (
+    <section class="w-full">
+      <Text as="h2">{props.title}</Text>
+      {props.children}
+    </section>
+  );
+};
