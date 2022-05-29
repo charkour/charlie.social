4919969e4ac12c3cca46bd3a88717e6e1b1b4e6a
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-05-29 16:34:09 +0200

    Add NavLink

---
+++ src/components/ui/NavLink.tsx
@@ -0,0 +1,18 @@
+import { type Component } from "solid-js";
+
+interface Props {
+  children: string;
+}
+
+export const NavLink: Component<Props> = (props) => {
+  return (
+    <li class="hover:italic relative">
+      <a
+        class="hover:before:content-['*'] before:absolute before:-left-2"
+        href={`/#${props.children}`}
+      >
+        {props.children}
+      </a>
+    </li>
+  );
+};
