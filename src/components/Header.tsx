586ff3267b5af51a15b4a93f9bce5c02d2ba3ab0
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-06-08 22:04:51 -0400

    add some flair

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { type Component, For } from "solid-js";
 import { Typography } from "./ui/Typography";
 import photo from "../assets/charles.jpg";
 import { Split } from "./ui/Split";
+import confetti from "canvas-confetti";
 
 export const Header: Component = () => {
   return (
@@ -13,9 +14,13 @@ export const Header: Component = () => {
         class="rounded-full ring-1 ring-offset-4 ring-gray-500 h-16 sm:h-24 md:h-32 lg:h-44 xl:h-64 cursor-pointer"
         alt="profile shot of Charles"
         title="profile shot of Charles"
+        onClick={() => confetti()}
       />
       <Split class="pt-1">Software Engineer</Split>
-      <p class='pt-2 mx-10 text-center'>I help companies modernize their tech stacks and create performant experiences users love.</p>
+      <p class="pt-2 mx-10 text-center">
+        I help companies modernize their tech stacks and create performant
+        experiences users love.
+      </p>
     </header>
   );
 };
