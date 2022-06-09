e3f53a9e79036fcf7bd31bf4a1a7623f2cead39d
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-06-09 17:49:33 -0400

    update links

--- src/data/links.ts
+++ src/data/links.ts
@@ -16,7 +16,13 @@ export const links = [
     href: "https://www.linkedin.com/in/charles-kornoelje/",
     title: "LinkedIn",
     color: "teal",
-    desc: "Probably to best way to reach out to me! I don't spend too much time on this platform, but it's a great way to see what my friends are up to in their professional lives!",
+    desc: "Probably to best way to reach out to me. I don't spend too much time on this platform, but it's a great way to see what my friends are up to in their professional lives.",
+  },
+  {
+    href: "https://www.strava.com/athletes/charkour",
+    title: "Strava",
+    color: "red",
+    desc: "See what a post-collegiate runner is up to.",
   },
   {
     href: "https://twitter.com/_charkour",
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,25 +1,29 @@
-import { Project } from "../components/Project";
+import { type ProjectProps } from "../components/Project";
 
-export const projects: Project[] = [
+export const projects: ProjectProps[] = [
   {
     title: "Tekton.com",
-    desc: "ecommerce doing MM in monthly D2C revenue",
+    desc: "6 month rewrite of Tekton.com using modern web technologies and best-of-breed microservices.",
     tech: ["Next.js", "React.js"],
+    href: "https://www.tekton.com",
   },
   {
     title: "Course Schedulizer",
     desc: "Project used internally at Calvin University by department chairs",
     tech: ["React.js", "Sass"],
+    href: "https://github.com/senior-knights/course-schedulizer",
   },
   {
     title: "Zundo",
     desc: "middleware for zustand. add time travel to your apps",
     tech: ["TypeScript", "React.js"],
+    href: "https://github.com/charkour/zundo",
   },
   {
     title: "React Reactions",
     desc: "Fork of an old npm package updated to work with modern React.js and Next.js",
     tech: ["React.js"],
+    href: "https://github.com/charkour/react-reactions",
   },
   {
     title: "Benchmark",
@@ -28,37 +32,40 @@ export const projects: Project[] = [
   },
   {
     title: "Turtle Tracker",
-    desc: "Mobile Application",
+    desc: "Mobile app used by the Calvin Ecosystem Preserve to track the health of their threatened box turtles",
     tech: ["React Native", "Expo", "Firebase", "PostgreSQL", "Ionic"],
+    href: "https://github.com/calvin-cs262-fall2019-teamH?type=source",
   },
   {
     title: "IT Inventory Tracker",
-    desc: "Internal app used at CQL",
+    desc: "Internal app used at CQL to keep track of their inventory.",
     tech: [".NET Core", "C#", "React.js"],
   },
   {
     title: "Vacation Tracker",
-    desc: "Restyled internal CQL app",
+    desc: "Restyled internal CQL HR vacation app 🌴",
     tech: ["Bulma", "Gulp"],
   },
   {
     title: "csps",
     desc: "tools to solve constraint satisfaction problems",
     tech: ["TypeScript"],
+    href: "https://github.com/charkour/csps",
   },
   {
     title: "Harmoniously",
     desc: "automatically create non-conflicting semester schedules for departments and universities",
     tech: ["TypeScript"],
+    href: "https://github.com/charkour/harmoniously",
   },
   {
     title: "Grit Leadership",
-    desc: "Coaching Evaluation",
+    desc: "Coaching Evaluation platform",
     tech: ["TypeScript", "React.js", "MUI"],
   },
   {
     title: "Calvin Orientation",
-    desc: "Mobile app",
+    desc: "Mobile app used by Calvin University to help students learn about the campus and complete Quest",
     tech: ["TypeScript", "React Native", "Expo"],
   },
 ];
