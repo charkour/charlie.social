1c869d454bc9b70f839320db1d483dd8fd3b9661
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-12-04 09:11:04 -0500

    fix favicon path

--- src/root.tsx
+++ src/root.tsx
@@ -34,7 +34,7 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta name="theme-color" content="#000" />
-        <Link rel="shortcut icon" type="image/ico" href="/src/assets/favicon.ico" />
+        <Link rel="shortcut icon" type="image/ico" href="/assets/favicon.ico" />
       </Head>
       <Body>
         <Suspense>
