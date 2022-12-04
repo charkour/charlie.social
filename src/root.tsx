733ee1dc0aa992d00b27f71e36ff6e3e46965cff
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-12-04 09:27:14 -0500

    remove suspense

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,5 @@
 /* @refresh reload */
-import { Suspense, onMount } from "solid-js";
+import { onMount } from "solid-js";
 import {
   Body,
   ErrorBoundary,
@@ -10,14 +10,16 @@ import {
   Routes,
   Scripts,
   Title,
-  Link
+  Link,
 } from "solid-start";
 import "./root.css";
 
 export default function Root() {
   onMount(() => {
     document.body.onmousemove = (e) => {
-      for (const card of document.getElementsByClassName("card") as HTMLCollectionOf<HTMLElement>) {
+      for (const card of document.getElementsByClassName(
+        "card"
+      ) as HTMLCollectionOf<HTMLElement>) {
         const rect = card.getBoundingClientRect(),
           x = e.clientX - rect.left,
           y = e.clientY - rect.top;
@@ -26,7 +28,7 @@ export default function Root() {
         card.style.setProperty("--y", `${y}px`);
       }
     };
-  })
+  });
   return (
     <Html lang="en">
       <Head>
@@ -34,17 +36,18 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta name="theme-color" content="#000" />
-        <Meta name="description" content="Performance engineer and web developer." />
+        <Meta
+          name="description"
+          content="Performance engineer and web developer."
+        />
         <Link rel="shortcut icon" type="image/ico" href="/assets/favicon.ico" />
       </Head>
       <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
+        <ErrorBoundary>
+          <Routes>
+            <FileRoutes />
+          </Routes>
+        </ErrorBoundary>
         <Scripts />
       </Body>
     </Html>
