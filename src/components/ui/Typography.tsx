086b8732c8d3a3e77fa892094cc0cc399fb4a79e
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-12-03 21:46:22 -0500

    use Dynamic

--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -1,25 +1,24 @@
 import { type Component, JSX } from "solid-js";
-import { spread, classList, setAttribute } from "solid-js/web";
+import { Dynamic } from "solid-js/web";
 
 interface Props extends JSX.HTMLAttributes<HTMLHeadingElement> {
   as?: Parameters<typeof document.createElement>[0];
 }
 
 export const Typography: Component<Props> = ({ as = "h1", ...rest }) => {
-  const element = document.createElement(as);
-  spread(element, rest);
-  classList(element, {
-    "uppercase font-bold text-3xl sm:text-5xl md:text-6xl border-b w-full text-center tracking-wide py-0.5 sm:py-1 md:py-2":
-      as === "h1",
-    "text-xl sm:text-3xl font-thin sm:leading-10":
-      as === "h2",
-    "text-lg sm:text-2xl font-bold":
-      as === "h3",
-    "": as === "small",
-    "text-sm sm:text-base leading-4": as === "p",
-  });
-  typeof rest.children === "string" &&
-    setAttribute(element, "id", rest.children);
-
-  return element;
+  return (
+    <Dynamic
+      component={as}
+      {...rest}
+      classList={{
+        "uppercase font-bold text-3xl sm:text-5xl md:text-6xl border-b w-full text-center tracking-wide py-0.5 sm:py-1 md:py-2":
+          as === "h1",
+        "text-xl sm:text-3xl font-thin sm:leading-10": as === "h2",
+        "text-lg sm:text-2xl font-bold": as === "h3",
+        "": as === "small",
+        "text-sm sm:text-base leading-4": as === "p",
+      }}
+      id={typeof rest.children === "string" ? rest.children : undefined}
+    />
+  );
 };
