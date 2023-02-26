a542b902c030561ada4c980657e16b960d8b72e0
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2023-02-26 14:35:06 -0500

    switch to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 // @ts-ignore -- no types
-import vercel from "solid-start-vercel";
+import netlify from 'solid-start-netlify'
 
 export default defineConfig({
   plugins: [
     solid({
       ssr: true,
-      adapter: vercel({
+      adapter: netlify({
         prerender: true,
       }),
     }),
