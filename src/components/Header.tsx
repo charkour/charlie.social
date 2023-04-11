3a3eb81bd8685a9e9c99dc92bacfc3b80794440d
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2023-04-10 23:17:22 -0400

    try fixing image shift

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header: Component = () => {
     <header class="flex flex-col items-center">
       <Typography>Charles Kornoelje</Typography>
       <div
-        class="overflow-hidden rounded-full ring-1 ring-offset-4 ring-gray-500 cursor-pointer h-20 sm:h-24 md:h-32 lg:h-44 xl:h-64"
+        class="overflow-hidden rounded-full ring-1 ring-offset-4 ring-gray-500 cursor-pointer h-20 sm:h-24 md:h-32 lg:h-44 xl:h-64 w-max"
         onClick={() => confetti()}
       >
         <img
