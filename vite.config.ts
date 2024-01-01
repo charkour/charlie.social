803b21d6fb6f10150e504317aa610d30d8630656
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2024-01-01 18:24:20 -0500

    switch to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   start: {
     server: {
-      preset: "netlify-static"
+      preset: "netlify"
     }
   }
 });
