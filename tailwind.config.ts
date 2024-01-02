776204f4424c870fbdfe5777897a9deb42da9554
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2024-01-01 19:42:52 -0500

    switch to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+const config: Config = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {}
   },
   plugins: []
 };
+export default config;
